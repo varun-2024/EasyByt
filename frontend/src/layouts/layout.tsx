96d7f882b8db508e8e863402503cab3abc0c7bb8
varun-2024 <v.talwar2024@gmail.com>
2025-08-07 23:51:24 +0100

    Including Header component

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -17,6 +17,7 @@ const layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
   );
 };
